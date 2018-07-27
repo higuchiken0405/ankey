a9fb138fcca8d7419446d317e7fbe73fec980a35
kenhiguchi0405 <higuchiken0405@gmail.com>
2018-07-27 17:56:16 +0900

    ユーザー編集ページ追加(途中)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   end
   # ユーザー登録画面のフォームデータを処理(サインアップ)
   def create
-    @user = User.new(users_params)
+    @user = User.new(user_params)
     if @user.save
       flash[:success] = "登録完了しました"
       redirect_to users_path
@@ -15,18 +15,34 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
   # ユーザー情報を一覧表示
   def index
     @users = User.all
   end
   # ユーザー情報ページ表示
   def show
-
+    @user = User.find_by(id: params[:id])
   end
 
 
+  # ユーザー情報編集ページ表示
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+  #
+  def update
+    @user = User.find_by(id: params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "編集が完了しました"
+      redirect_to user_path(id: params[:id])
+    else
+      flash[:notice] = "編集に失敗しました"
+      render :edit
+    end
+  end
 private
-  def users_params
+  def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   validates :email, presence: true
 
   has_secure_password
+  validates :password, presence:true, length: { minimum: 5}
 end
