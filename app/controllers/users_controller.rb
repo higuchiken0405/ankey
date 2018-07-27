9e735ab52b0a446ad33f5e0e3c1480f11d4ae046
kenhiguchi0405 <higuchiken0405@gmail.com>
2018-07-27 14:06:58 +0900

    パスワード機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
 
 private
   def users_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
 end
