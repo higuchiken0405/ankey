3f8b65de3d0fb2bd53ba98c19437f36067ca7077
kenhiguchi0405 <higuchiken0405@gmail.com>
2018-07-27 17:07:54 +0900

    jQuery導入、flash設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,27 @@
 class UsersController < ApplicationController
 
+  # ユーザー登録画面
   def new
     @user = User.new
   end
-
+  # ユーザー登録画面のフォームデータを処理(サインアップ)
   def create
     @user = User.new(users_params)
     if @user.save
       flash[:success] = "登録完了しました"
-      redirect_to root_path
+      redirect_to users_path
     else
       flash.now[:notice] = "登録に失敗しました"
       render :new
     end
+  end
+  # ユーザー情報を一覧表示
+  def index
+    @users = User.all
+  end
+  # ユーザー情報ページ表示
+  def show
+
   end
 
 
