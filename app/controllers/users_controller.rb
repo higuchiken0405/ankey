fd1f1ba4e29478c5990bedfb7aba2bb0a017adb1
kenhiguchi0405 <higuchiken0405@gmail.com>
2018-07-27 13:36:03 +0900

    Usercontroller作成ルーティング設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,24 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
-  
+
+  def create
+    @user = User.new(users_params)
+    if @user.save
+      flash[:success] = "登録完了しました"
+      redirect_to root_path
+    else
+      flash.now[:notice] = "登録に失敗しました"
+      render "new"
+    end
+  end
+
+
+private
+  def users_params
+    params.require(:user).permit(:name, :email)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
 
   root "pages#index"
   get "/signup",  to: "users#new"
+  resources :users
 end
